7a86629e748b2f57349045b6975b5f96864c978c
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-09-15 15:13:23 -0300

    add explicit entry point and text reader method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Teleprompter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = ReadFrom("sampleQuotes.txt");
+
+        foreach (var line in lines)
+        {
+            Console.Write(line);
+
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                var pause = Task.Delay(200);
+
+                // Synchronously waiting on a task is an
+                // anti-pattern. This will get fixed in later
+                // steps.
+
+                pause.Wait();
+            }
+        }
+    }
+
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+
+        using (var reader = File.OpenText(file))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                var words = line.Split(' ');
+                var lineLength = 0;
+
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                    lineLength += word.Length + 1;
+
+                    if (lineLength > 70)
+                    {
+                        yield return Environment.NewLine;
+                        lineLength = 0;
+                    }
+                }
+
+                yield return Environment.NewLine;
+            }
+        }
+    }
+}
