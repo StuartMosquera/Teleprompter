b5ca2ddc906b340e4f5f0c585aaf651e6e61e0f3
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-09-25 12:43:03 -0300

    refactor to follow official best practices

--- Program.cs
+++ Program.cs
@@ -2,31 +2,27 @@
 
 internal class Program
 {
-    static async Task Main(string[] args)
-    {
-        await RunTeleprompter();
-    }
+    private static async Task Main(string[] args) => await RunTeleprompter();
 
     private static async Task RunTeleprompter()
     {
         var config = new TeleprompterConfig();
-        var displayTask = ShowTeleprompter(config);
-        var speedTask = GetInput(config);
-        await Task.WhenAny(displayTask, speedTask);
+        Task display = ShowTeleprompter(config);
+        Task speed = GetInput(config);
+
+        await Task.WhenAny(display, speed);
     }
 
     private static async Task ShowTeleprompter(TeleprompterConfig config)
     {
-        var words = ReadFrom("sampleQuotes.txt");
+        IEnumerable<string> words = ReadFrom("sampleQuotes.txt");
 
-        foreach (var word in words)
+        foreach (string word in words)
         {
             Console.Write(word);
 
             if (!string.IsNullOrWhiteSpace(word))
-            {
                 await Task.Delay(config.DelayInMilliseconds);
-            }
         }
 
         config.SetDone();
@@ -34,55 +30,50 @@ internal class Program
 
     private static async Task GetInput(TeleprompterConfig config)
     {
-        Action work = () =>
+        void Work()
         {
             do
             {
-                var key = Console.ReadKey(true);
+                ConsoleKeyInfo key = Console.ReadKey(true);
 
                 if (key.KeyChar == '>')
-                {
                     config.UpdateDelay(-10);
-                }
-                else if (key.KeyChar == '<')
-                {
-                    config.UpdateDelay(10);
-                }
-                else if (key.KeyChar == 'X' || key.KeyChar == 'x')
-                {
-                    config.SetDone();
-                }
-            } while (!config.Done);
-        };
 
-        await Task.Run(work);
+                if (key.KeyChar == '<')
+                    config.UpdateDelay(10);
+
+                if ((key.KeyChar == 'X') || (key.KeyChar == 'x'))
+                    config.SetDone();
+            } while (!config.Done);
+        }
+
+        await Task.Run(Work);
     }
 
-    static IEnumerable<string> ReadFrom(string file)
+    private static IEnumerable<string> ReadFrom(string file)
     {
         string? line;
 
-        using (var reader = File.OpenText(file))
+        using StreamReader reader = File.OpenText(file);
+
+        while ((line = reader.ReadLine()) != null)
         {
-            while ((line = reader.ReadLine()) != null)
+            string[] words = line.Split(' ');
+            var lineLength = 0;
+
+            foreach (string word in words)
             {
-                var words = line.Split(' ');
-                var lineLength = 0;
+                yield return word + " ";
+                lineLength += word.Length + 1;
 
-                foreach (var word in words)
+                if (lineLength > 70)
                 {
-                    yield return word + " ";
-                    lineLength += word.Length + 1;
-
-                    if (lineLength > 70)
-                    {
-                        yield return Environment.NewLine;
-                        lineLength = 0;
-                    }
+                    yield return Environment.NewLine;
+                    lineLength = 0;
                 }
-
-                yield return Environment.NewLine;
             }
+
+            yield return Environment.NewLine;
         }
     }
 }
--- TeleprompterConfig.cs
+++ TeleprompterConfig.cs
@@ -2,20 +2,17 @@ using static System.Math;
 
 namespace Teleprompter;
 
-internal class TeleprompterConfig
+public class TeleprompterConfig
 {
     public int DelayInMilliseconds { get; private set; } = 200;
     public bool Done { get; private set; }
 
     public void UpdateDelay(int increment) // Negative to speed up
     {
-        var newDelay = Min(DelayInMilliseconds + increment, 1000);
+        int newDelay = Min(DelayInMilliseconds + increment, 1000);
         newDelay = Max(newDelay, 25);
         DelayInMilliseconds = newDelay;
     }
-
-    public void SetDone()
-    {
-        Done = true;
-    }
+    
+    public void SetDone() => Done = true;
 }
