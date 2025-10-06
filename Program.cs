0b34434907a98333215855eeafe1738a998b49d1
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-10-06 12:12:32 -0300

    order 'Program' class members

--- Program.cs
+++ Program.cs
@@ -2,54 +2,6 @@
 
 internal class Program
 {
-    private static async Task Main(string[] args) => await RunTeleprompter();
-
-    private static async Task RunTeleprompter()
-    {
-        var config = new TeleprompterConfig();
-        Task display = ShowTeleprompter(config);
-        Task speed = GetInput(config);
-
-        await Task.WhenAny(display, speed);
-    }
-
-    private static async Task ShowTeleprompter(TeleprompterConfig config)
-    {
-        IEnumerable<string> words = ReadFrom("sampleQuotes.txt");
-
-        foreach (string word in words)
-        {
-            Console.Write(word);
-
-            if (!string.IsNullOrWhiteSpace(word))
-                await Task.Delay(config.DelayInMilliseconds);
-        }
-
-        config.SetDone();
-    }
-
-    private static async Task GetInput(TeleprompterConfig config)
-    {
-        void Work()
-        {
-            do
-            {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-
-                if (key.KeyChar == '>')
-                    config.UpdateDelay(-10);
-
-                if (key.KeyChar == '<')
-                    config.UpdateDelay(10);
-
-                if ((key.KeyChar == 'X') || (key.KeyChar == 'x'))
-                    config.SetDone();
-            } while (!config.Done);
-        }
-
-        await Task.Run(Work);
-    }
-
     private static IEnumerable<string> ReadFrom(string file)
     {
         string? line;
@@ -75,5 +27,56 @@ internal class Program
 
             yield return Environment.NewLine;
         }
+    }
+
+    private static async Task GetInput(TeleprompterConfig config)
+    {
+        void Work()
+        {
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.KeyChar == '>')
+                    config.UpdateDelay(-10);
+
+                if (key.KeyChar == '<')
+                    config.UpdateDelay(10);
+
+                if ((key.KeyChar == 'X') || (key.KeyChar == 'x'))
+                    config.SetDone();
+            } while (!config.Done);
+        }
+
+        await Task.Run(Work);
+    }
+
+    private static async Task ShowTeleprompter(TeleprompterConfig config)
+    {
+        IEnumerable<string> words = ReadFrom("sampleQuotes.txt");
+
+        foreach (string word in words)
+        {
+            Console.Write(word);
+
+            if (!string.IsNullOrWhiteSpace(word))
+                await Task.Delay(config.DelayInMilliseconds);
+        }
+
+        config.SetDone();
+    }
+
+    private static async Task RunTeleprompter()
+    {
+        var config = new TeleprompterConfig();
+        Task display = ShowTeleprompter(config);
+        Task speed = GetInput(config);
+
+        await Task.WhenAny(display, speed);
+    }
+
+    private static async Task Main()
+    {
+        await RunTeleprompter();
     }
 }
